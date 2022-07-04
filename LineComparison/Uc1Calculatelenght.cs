2cb39b2179edee6826b2c50254809c736c6bc790
rajashri15 <rajashristelvelar315@gmail.com>
2022-07-04 10:24:19 +0530

    [Rajashri]-Added UC1 Code-Calculate lenght of one line

---
+++ LineComparison/Uc1Calculatelenght.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    public class Uc1Calculatelenght
+    {
+        public static void CalculateLength()
+        {
+            Console.WriteLine("Enter value of X1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of X2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of Y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of Y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("--------------------Calculate Lenght of one Line-----------------------");
+            Console.WriteLine(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
+
+            Console.WriteLine("--------2 decimal point");
+            double result= Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(Math.Round(result, 2));
+        }
+    }
+}
